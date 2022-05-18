98ac04c4d0c25825c4652dff7121e11d2ba75cc3
czomii@gmail.com <czomii@gmail.com>
2022-05-18 14:28:02 +0200

    zadania

---
+++ com/zadania/wiek/ReturnsAge.java
@@ -0,0 +1,11 @@
+package com.zadania.wiek;
+
+public class ReturnsAge {
+    static int age = 21;
+    public static void main(String[] args) {
+        System.out.println(getage());
+    }
+    static int getage() {
+        return age;
+    }
+}
