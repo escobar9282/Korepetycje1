7587f7a1a4c97880480245f0b5f9fdd1a192051b
czomii@gmail.com <czomii@gmail.com>
2022-05-29 18:28:44 +0200

    zadania

---
+++ com/zadania/zadanie4_1/Main.java
@@ -0,0 +1,12 @@
+package com.zadania.zadanie4_1;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] tab = new int[10];
+        for (int i = 0; i <= 9; i++){
+            tab[i] = i;
+            System.out.print(tab[i]);
+            System.out.print(" ");
+        }
+    }
+}
