4d3cb2ab2541e15b9b65fdf24d9aaeb0240a3caa
czomii@gmail.com <czomii@gmail.com>
2022-07-18 01:57:16 +0200

    zadania

---
+++ com/zadania/zadanie7_1/Main.java
@@ -0,0 +1,7 @@
+package com.zadania.zadanie7_1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ com/zadania/zadanie7_1/ThreadWithExtension.java
@@ -0,0 +1,8 @@
+package com.zadania.zadanie7_1;
+
+public class ThreadWithExtension extends Thread{
+    public void run() {
+        System.out.println("My first thread ");
+    }
+}
+
